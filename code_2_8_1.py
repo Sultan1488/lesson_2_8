7ec2ffd413de1a726b068f7753ba17786da67405
Sultan <sultan@MacBook-Pro.local>
2021-05-04 19:38:08 +0600

    initial commit

---
+++ code_2_8_1.py
@@ -0,0 +1,12 @@
+class Video_game:
+    name = 'Witcher 3'
+    developers = 'CD Project'
+    main_character = 'Geralt 0f Rivia'
+
+    def start(self):
+        print('The game is running')
+
+    def stop(self):
+        print('The game is off')
+
+first_copy_game = Video_game()
