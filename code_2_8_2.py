3537da4a3b4486135a49443a3c7650bbcc9c451c
Sultan <sultan@MacBook-Pro.local>
2021-05-04 19:43:47 +0600

    initial commit

---
+++ code_2_8_2.py
@@ -0,0 +1,12 @@
+class Anime:
+    name = 'The seven deadly sins'
+    main_character = 'Meliodas'
+    num_seasons = 4
+
+    def start(self):
+        print('The anime is running')
+
+    def stop(self):
+        print('The anime is off')
+
+first_copy_anime = Anime()
